52294cfea90c15bd7395b156fde49567c9a8ffdb
jeff smith <48994532+matryoshkev@users.noreply.github.com>
2020-04-14 08:32:45 -0500

    Quick look at testing data

--- 01-get-data.R
+++ 01-get-data.R
@@ -9,8 +9,7 @@ library("lubridate")
 # Get case testing data from The Covid Tracking Project 
 # (https://covidtracking.com, a project of The Atlantic)
 
-testing_url  <- paste0("https://covidtracking.com/api/v1/states/daily.csv")
-testing_data <- RCurl::getURL(testing_url)
+testing_data <- RCurl::getURL("https://covidtracking.com/api/v1/states/daily.csv")
 testing_data <- readr::read_csv(testing_data)
 testing_data <- testing_data %>% 
 	select(date, state, positive, negative, totalTestResults) %>%
@@ -22,5 +21,14 @@ write_csv(testing_data, "data/data-testing.csv")
 library("ggplot2")
 qplot(x = date, y = totalTestResults, color = state, data = testing_data)
 qplot(x = date, y = totalTestResults, color = state, data = testing_data, log = "y")
-qplot(x = date, y = positive/totalTestResults, color = state, data = testing_data)
+qplot(x = date, y = negative, color = state, data = testing_data, log = "y")
+qplot(
+	x = date, y = positive/totalTestResults, color = state, 
+	data = testing_data, ylim = c(0, 0.25)
+)
+# MO didn't start reporting good negative test numbers until 2020-03-28
+# IL numbers stabilize at 2020-03-29
+
+# Clean up
+rm(testing_data)
 
